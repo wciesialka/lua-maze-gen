bb32ca2e417d3e4f2f572ec32285f04c9cda3a65
William Ciesialka <wciesialka@gmail.com>
2020-05-29 18:44:59 -0400

    Changed name of classify to vtochar

--- Main.lua
+++ Main.lua
@@ -6,6 +6,6 @@ if size == nil then size = 5 end
 maze = GenerateMaze(size,false)
 
 for i,v in pairs(maze) do
-    io.write(classify(v))
+    io.write(vtochar(v))
     if i % size == 0 then print() end
 end
